9891e6a6d74a97321e3145fd4d9ca716458cafb0
j-emman <j.emmanuel.rubion@outlook.com>
2024-08-06 10:04:08 +0800

    Update
    Test

--- Forms/ActivityManager_Form.Designer.cs
+++ Forms/ActivityManager_Form.Designer.cs
@@ -552,7 +552,7 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(288, 231);
+            this.button2.Location = new System.Drawing.Point(642, 231);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(103, 40);
             this.button2.TabIndex = 71;
